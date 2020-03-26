b089a8d3040111997613b94841750488de3953ce
jayantdwivedi <jayantdwivedi19@gmail.com>
2020-03-26 21:57:28 +0530

    Problem2 Solved.

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,27 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(45);
+        tree.insert(75);
+        tree.insert(23);
+        tree.insert(34);
+        tree.insert(65);
+        tree.insert(76);
+        tree.insert(89);
+
+        System.out.println("Post Order Traversal");
+        tree.traversePostOrder(tree.getRoot());
+
+        System.out.println();
+        System.out.println("Pre Order Traversal");
+        tree.traversePreOrder(tree.getRoot());
+    }
 }

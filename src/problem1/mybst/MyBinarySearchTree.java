ca40c2bc34a275892bc46a94b85304bdb9dc926c
jayantdwivedi <jayantdwivedi19@gmail.com>
2020-03-26 22:24:53 +0530

    Modification and updation in Classes.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -8,61 +8,90 @@ package problem1.mybst;
 
 import problem1.node.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
 
-    TreeNode root;
+    TreeNode newnode, root, tmp;
+    int ctr;
+    Queue<Integer> queue;
 
-    public TreeNode getRoot(){
+    public MyBinarySearchTree() {
+        tmp = null;
+        root = null;
+        ctr = 0;
+        queue = new LinkedList<>();
+    }
+
+    //node generation
+    private void generateNode(int data)
+    {
+        newnode=new TreeNode();
+        newnode.setData(data);
+        newnode.setLeft(null);
+        newnode.setRight(null);
+    }
+    //seeting root node
+    public void setRoot(int data)
+    {
+        generateNode(data);
+        if(root==null)
+        {
+            root=newnode;
+            tmp=root;
+            newnode=null;
+
+        }
+    }
+    public TreeNode getRoot() {
         return root;
     }
-
-    // to insert
-
-    public void insert(int data){
-        TreeNode node = new TreeNode(data);
-        if(root == null){
-            root = node;
+    //setting binary tree
+    public void insert(TreeNode tmproot) {
+        if (newnode == null) {
+            generateNode(new Scanner(System.in).nextInt());
+            System.out.println("Node generated");
         }
-        else{
-            TreeNode temp = root;
-            TreeNode parent = null;
-            while(temp != null){
-                parent = temp;
-                if(node.getData() <= temp.getData()){
-                    temp = temp.getLeft();
+        try {
+            if (newnode.getData() <= tmproot.getData()) {
+                System.out.println("left traversal...");
+                if (tmproot.getLeft() == null) {
+                    tmproot.setLeft(newnode);
+                    System.out.println("node inserted left");
+                    queue.add(newnode.getData());
+                    ctr--;
+                    newnode = null;
+                    return;
+                } else {
+                    System.out.println("left not empty changing tmproot ");
+                    insert(tmproot.getLeft());
                 }
-                else{
-                    temp = temp.getRight();
+            }
+        }catch(NullPointerException ignore){}
+        try {
+            if (newnode.getData() > tmproot.getData()) {
+                System.out.println("Right traversal...");
+                if (tmproot.getRight() == null) {
+                    tmproot.setRight(newnode);
+                    System.out.println("Node inserted right");
+                    ctr++;
+                    newnode = null;
+                } else {
+                    System.out.println("Right not empty changing tmproot");
+                    insert(tmproot.getRight());
                 }
             }
-
-            if(node.getData() <= parent.getData()){
-                parent.setLeft(node);
-            }
-            else{
-                parent.setRight(node);
-            }
+        } catch (NullPointerException ignore) {
         }
     }
 
-    // to Traverse PreOrder
-
-    public void traversePreOrder(TreeNode node){
-        if(node != null){
-            System.out.print(node.getData() + ",");
-            traversePreOrder(node.getLeft());
-            traversePreOrder(node.getRight());
-        }
-    }
-
-    // to TraversePost Order
-
-    public void traversePostOrder(TreeNode node){
-        if(node != null){
-            traversePostOrder(node.getRight());
-            System.out.print(node.getData() + ",");
-            traversePostOrder(node.getLeft());
-        }
+    //Answer to questions
+    public void printLeftChildren() {
+        System.out.println("The left children of inserted nodes of Binary search tree are :");
+        System.out.println(queue.toString());
+        System.out.println("Number of Nodes not having left children" + ctr);
     }
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,22 +7,30 @@
 package problem1.node;
 
 public class TreeNode {
-    private int data;
     private TreeNode left;
     private TreeNode right;
-
-    public TreeNode(int data) {
+    private int data;
+    private TreeNode next;
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
         this.data = data;
+        next=null;
+    }
+    //default constructor
+    public TreeNode() {
         this.left = null;
         this.right = null;
+        this.data = 0;
+        next=null;
     }
 
-    public int getData() {
-        return data;
+    public void setNext(TreeNode next) {
+        this.next = next;
     }
 
-    public void setData(int data) {
-        this.data = data;
+    public TreeNode getNext() {
+        return next;
     }
 
     public TreeNode getLeft() {
@@ -39,6 +47,14 @@ public class TreeNode {
 
     public void setRight(TreeNode right) {
         this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
     }
 
 }

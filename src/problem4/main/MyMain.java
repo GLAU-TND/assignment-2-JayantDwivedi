615263ba0fc0ed9678af08b1f6ffe0a05ca0c0d2
jayantdwivedi <jayantdwivedi19@gmail.com>
2020-03-28 08:06:03 +0530

    problem 4 solved with node and Student

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,30 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        MyBinarySearchTree m=new MyBinarySearchTree();
+        MyQueue q=new MyQueue();
+        //setting up the root
+        m.setRoot(sc.nextInt());
+        //inserting into the tree
+        for (int i = 0; i <5 ; i++)
+        {
+            m.insert(m.getRoot());
+        }
+        //method to print preorder Successor of
+        //Answer
+        System.out.println("Enter value of which you want to find preorder Successor");
+        q.printSuccessor(sc.nextInt());
+    }
 }
